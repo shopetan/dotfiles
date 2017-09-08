5d649573e6b1d68f25b3ea954e870a72bb724e88
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-09-09 03:17:45 +0900

    お掃除

---
+++ emacs/.emacs.d/elpa/auto-complete-rst-20140225.144/auto-complete-rst-autoloads.el
@@ -0,0 +1,16 @@
+;;; auto-complete-rst-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("auto-complete-rst-pkg.el" "auto-complete-rst.el")
+;;;;;;  (22962 51898 942432 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; auto-complete-rst-autoloads.el ends here
